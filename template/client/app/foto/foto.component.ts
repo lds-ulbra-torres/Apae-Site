d33138fea0e6cf4e4259c33e2f312ca06abcc2ff
DanielaSchutt <daniela1schutt@gmail.com>
2018-04-04 10:02:25 -0300

    template básico

---
+++ template/client/app/foto/foto.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'foto',
+    templateUrl: './app/foto/foto.component.html' 
+})
+export class FotoComponent {
+}
---
+++ template/client/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home',
+    templateUrl: './app/home/home.component.html'
+})
+export class HomeComponent {
+    private title = 'Template Básico';
+    private subTitle = 'APAE';
+}
