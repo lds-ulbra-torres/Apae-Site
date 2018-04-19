70df5df57a0d466a68d89b1d9c442d51ffa844fd
DanielaSchutt <daniela1schutt@gmail.com>
2018-04-19 14:00:48 -0300

    Melhorias

---
+++ template/src/app/foto/foto.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foto',
+  templateUrl: './foto.component.html',
+  styleUrls: ['./foto.component.css']
+})
+export class FotoComponent {
+
+}
---
+++ template/src/app/foto/foto.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FotoComponent} from './foto.component';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        FotoComponent
+    ],
+    exports: [
+        FotoComponent
+    ]
+})
+export class FotoModule { }
