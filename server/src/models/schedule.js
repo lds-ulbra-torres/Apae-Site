7fce20dc5bcc1fad9ea09930690c65e74a679e12
JozielOliveira <programdor.joziel@gmail.com>
2018-06-04 19:12:00 -0300

    validate date

--- server/src/models/schedule.js
+++ server/src/models/schedule.js
@@ -11,14 +11,16 @@ export default (sequelize, DataType) => {
         type: DataType.STRING,
         allowNull: false,
         validate: {
-          notEmpty: true
+          notEmpty: true,
+          is: ["(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/[12][0-9]{3}",'g'] 
         }
       },
       hour: {
         type: DataType.STRING,
         allowNull: false,
         validate: {
-          notEmpty: true
+          notEmpty: true,
+          is: ["([0-1][0-9]|[2][0-3]):(0[1-9]|[1-5][1-9]):(0[1-9]|[1-5][1-9])",'g']
         }
       }
     },
